262032fa56233720fd4d551ed64150abf7c566a2
SvetlanaHolovko <svetta.gsn@gmail.com>
2022-01-29 13:28:13 +0200

    Updates Mogo

--- Mogo/js/app.js
+++ Mogo/js/app.js
@@ -1,15 +1,16 @@
-$(function() {
+$(function () {
 
 	var header = $("#header"),
-	    // переменная с высотой блока intro
-	    introH = $("#intro").innerHeight(),
-	    // текущий скрол при загрузке страницы
-	    scrollOffset = $(window).scrollTop();	
-    /* Fixed Header*/
-	    // запускаем функцию
-		checkScroll(scrollOffset);
+		// переменная с высотой блока intro
+		introH = $("#intro").innerHeight(),
+		// текущий скрол при загрузке страницы
+		scrollOffset = $(window).scrollTop();
+
+	/* Fixed Header*/
+	// запускаем функцию
+	checkScroll(scrollOffset);
 	// скролим страницу
-	$(window).on("scroll", function() {
+	$(window).on("scroll", function () {
 		// обновляем значение скрола на сколько проскролили
 		scrollOffset = $(this).scrollTop();
 		// передаем в функцию новое значение
@@ -18,7 +19,7 @@ $(function() {
 
 	function checkScroll(scrollOffset) {
 		// если высота скрола > intro, то header.fixed
-		if( scrollOffset >= introH ) {
+		if (scrollOffset >= introH) {
 			header.addClass("fixed");
 		} else {
 			header.removeClass("fixed");
@@ -26,25 +27,25 @@ $(function() {
 	}
 
 	/* Smooth scroll */
-	$("[data-scroll]").on("click", function(event) {
+	$("[data-scroll]").on("click", function (event) {
 		event.preventDefault();
 		// получаем значение id из data-scroll
 		var $this = $(this),
-		    blockId = $this.data('scroll'),
-		    // получаем позицию элемента от верха страницы
-		    blockOffset = $(blockId).offset().top;
+			blockId = $this.data('scroll'),
+			// получаем позицию элемента от верха страницы
+			blockOffset = $(blockId).offset().top;
 		// убираем класс у неактивных ссылок
-		$("#nav a").removeClass("active");    
+		$("#nav a").removeClass("active");
 		// придаем нашей ссылке класс active
-		$this.addClass("active");    
+		$this.addClass("active");
 		// плавно переходим на страницу
-		$("html, body").animate( {
+		$("html, body").animate({
 			scrollTop: blockOffset
-		}, 500);   
+		}, 500);
 	})
 
 	/* Menu nav togle (бургер-меню) */
-	$("#nav_toggle").on("click", function(event) {
+	$("#nav_toggle").on("click", function (event) {
 		event.preventDefault();
 		// бургер превращаем в крестик
 		$(this).toggleClass("active");
@@ -52,11 +53,11 @@ $(function() {
 	})
 
 	/* Collasce (Аккордеон) */
-	$("[data-collapse]").on("click", function(event) {
+	$("[data-collapse]").on("click", function (event) {
 		event.preventDefault();
 		var $this = $(this),
-		    blockId = $this.data('collapse');
-		    $this.toggleClass("active");
+			blockId = $this.data('collapse');
+		$this.toggleClass("active");
 	})
 
 	/* Slider (Слайдер) */
@@ -64,7 +65,7 @@ $(function() {
 		infinite: true,
 		fade: false,
 		slidesToShow: 1,
-        slidesToScroll: 1
+		slidesToScroll: 1
 	});
 
 });
